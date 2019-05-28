2f510b8b7256b0479fc2d018524a5cbacded7ca5
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-28 10:18:14 +0300

    axios configuring

--- recordstore-frontend/src/main.js
+++ recordstore-frontend/src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import {
+  securedAxiosInstance,
+  plainAxiosInstance
+} from './backend/axios'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
+
 new Vue({
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   render: h => h(App)
 }).$mount('#app')
